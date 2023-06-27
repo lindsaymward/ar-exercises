afd9591b07d2a1ef831f684d02b2d5a1465e4ad0
Lindsay Ward <lward23@hotmail.com>
2023-06-26 17:05:47 -0700

    Connect employees to stores and generate employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 50)
+@store2.employees.create(first_name: "Christian", last_name: "Nally", hourly_rate: 55)
+@store2.employees.create(first_name: "Francis", last_name: "Bourgouin", hourly_rate: 55)
+@store2.employees.create(first_name: "Gary", last_name: "Jipp", hourly_rate: 45)
+@store4.employees.create(first_name: "Lindsay", last_name: "Ward", hourly_rate: 35)
